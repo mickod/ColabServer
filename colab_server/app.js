918acf66d945e0a07c6d6954d47e24525d32cbff
Mick <mick.odoherty@amodtech.com>
2015-01-19 20:42:10 +0100

    Initial commit of combined Node and angular components

---
+++ colab_server/app.js
@@ -0,0 +1,149 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var fs = require('fs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'uploaded_videos')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ------------------------------- ROUTES: steering GET, POST etc requests -------------------------------
+
+// get an instance of router
+var router = express.Router();
+
+// Enable CORS - cross domain routing
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
+// GET: home page route 
+router.get('/', function(req, res) {
+	res.send('Colab Server default nodejs home page - you shouldnt really be here ...');	
+});
+
+// GET: videoViewerPage
+router.get('/viewvideos', function(req, res) {
+        res.sendfile(__dirname + '/public/viewvideos.html');
+});
+
+// POST: video upload route
+// multer approach
+var multer = require('multer');
+app.use(multer({
+	
+	//Set dstination directory
+	dest: path.resolve(__dirname, 'public', 'uploaded_videos'),
+	
+	//Rename file
+	rename: function (fieldname, filename) {
+		//Add the current date and time in ISO format, removing the last 'Z' character this usually
+		//includes
+		var dateNow = new Date();
+	    return filename + "_" + dateNow.toISOString().slice(0,-1)
+	}
+
+}));
+
+router.post('/web_video_upload', function(req, res) {
+	res.send('Video Uploading');
+	console.dir(req.files);
+});
+
+// GET: route to return list of upload videos 
+router.get('/video_list', function(req, res) {
+	// Get the path for the uploaded_video directory - in a real app the video list would likely be taken from 
+	// a database index table, but this is fine for us for now
+	var _p;
+    _p = path.resolve(__dirname, 'public', 'uploaded_videos');
+	
+	//Find all the files in the diectory and add to a JSON list to return
+	var resp = [];
+	fs.readdir(_p, function(err, list) {
+		//Check if the list is undefined or empty first and if so just return 
+		if ( typeof list == 'undefined' || !list ) {
+			return;
+		}
+		for (var i = list.length - 1; i >= 0; i--) {
+			
+			// For each file in the directory add an id and filename to the response
+			console.log('Looping through video files in directory: ', list, 'at index: ', i);
+			console.dir(list);
+	    	resp.push( 
+				{"index": i,
+				"file_name": list[i]}
+			);
+			console.log('resp at iteration: ', i, " is: ", resp);
+	    }
+		
+		// Set the response to be sent
+		res.json(resp);
+	});
+});
+
+// DELETE: remove a video from the uploaded folder
+router.delete('/uploaded_videos/:id', function(req, res) {
+	console.dir("function: delete_video: " + req.params.id);
+	//Delete the video from the uploaded videos folder
+	var _p;
+    _p = path.resolve(__dirname, 'public', 'uploaded_videos', req.params.id);
+	fs.unlink(_p);
+});
+
+// apply the routes to our application
+app.use('/', router);
+app.use('/users', users);
+
+//----------------------------------------------------------------------------------------------------------
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ colab_server/public/js/main.js
@@ -0,0 +1,158 @@
+/** Main AngularJS Web Application */ 
+var app = angular.module('colabVideoServerWebApp', [ 'ngRoute', 'colabConfig', 'angularFileUpload']); 
+
+/** Configuration **/
+
+// Configure the Routes  
+app.config(['$routeProvider', function ($routeProvider) { 
+	$routeProvider 
+		// Home 
+		.when("/", {templateUrl: "partials/video_list.html", controller: "PageCtrl"}) 
+		
+		// Pages 
+		.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+		.when("/video_list", {templateUrl: "partials/video_list.html", controller: "PageCtrl"})
+		.when("/play_test_video", {templateUrl: "partials/play_test_video.html", controller: "PageCtrl"})
+		.when("/web_video_upload", {templateUrl: "partials/web_video_upload.html", controller: "WebUploadCtrl"})
+		
+		// else 404 
+		.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"}); 
+}]);
+
+// configure trusted source URL's
+app.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+ $sceDelegateProvider.resourceUrlWhitelist([
+   // Allow same origin resource loads.
+   'self',
+   // Allow loading from all domains for now - not secure in production env.
+   '**']);
+ }]);
+
+
+/** Controllers */
+
+// Controls Pages
+app.controller('PageCtrl', function (/*$scope, colabConfig*/) { 
+	console.log("Page Controller reporting for duty."); 
+
+	// Activates the Carousel 
+	$('.carousel').carousel({
+		 interval: 5000 }); 
+
+});
+
+// Controls video list page
+app.controller('VideoListCtrl', ['$scope', '$http', 'GetUploadedVideosFactory', 'colabConfig', function ($scope, $http, GetUploadedVideosFactory, colabConfig)  { 
+	console.log("VideoList controller"); 
+	
+	$scope.videoList = [];
+	
+	// Get the video list from the Colab Server
+	GetUploadedVideosFactory.getVideoList().then(function(data) {
+
+		// Should really do some type checking etc here on the returned value
+	   	console.dir(data.data);
+		
+		$scope.videoList = data.data;
+	});
+	
+	// Set the video player window to blank by default
+	$scope.playerWindowURL = './templates/empty_player_window.html';
+	
+	// function called when user clicks the play button for a particular video
+	function playVideo(video) {
+		console.log("playVideo button clicked"); 
+		// set the video player window to the player window instead of blank
+		$scope.playerWindowURL = './templates/video_player_window.html';
+		$scope.collabServerPlayVideoURL = colabConfig.colabServerBaseURL + "/uploaded_videos/" + video.file_name;
+	};
+	
+	// function called when user clicks the delet button for a particular video
+	function deleteVideo(video) {
+		console.log("deleteVideo button clicked");
+		// Send message to the server to delete the video
+		$http.delete(colabConfig.colabServerBaseURL + "/uploaded_videos/" + video.file_name);
+		
+		// Remove the video from the list being displayed
+		$scope.videoList.splice( $scope.videoList.indexOf(video), 1 );
+	};
+	
+	//Tell the scope about the functions in this controller
+	$scope.playVideo = playVideo;
+	$scope.deleteVideo = deleteVideo;
+
+}]);
+
+// Controls video test play div
+app.controller('VideoPlayTestCtrl', ['$scope', 'colabConfig', function ($scope, colabConfig)  { 
+	console.log("VideoList controller"); 
+	
+	//Set the base URL for the server
+	$scope.collabServerBaseURL = colabConfig.colabServerBaseURL;
+	$scope.collabServerTestVideoURL = colabConfig.colabServerBaseURL + "/videos/BigBuckBunny_320x180.mp4";
+
+}]);
+
+// Controls video upload from web (i.e. not from a mobile app) - uses module and apraoch at: https://github.com/danialfarid/angular-file-upload
+app.controller('WebUploadCtrl',[ '$scope', '$upload','colabConfig', function($scope, $upload, colabConfig) {
+	console.log("WebUploadCtrl controller");
+	$scope.progressPerCent = 0;
+	$scope.onFileSelect = function($files) {
+		console.log("file selected for upload...");
+		//$files: an array of files selected, each file has name, size, and type.
+		for (var i = 0; i < $files.length; i++) {
+		  var file = $files[i];
+		  $scope.upload = $upload.upload({
+		    url: colabConfig.colabServerBaseURL + "/web_video_upload",
+		    method: 'POST',
+		    //headers: {'header-key': 'header-value'},
+		    //withCredentials: true,
+		    data: {myObj: $scope.myModelObj},
+		    file: file,
+		  }).progress(function(evt) {
+			  console.log("progress: " + (evt.position/evt.totalSize)*100);
+			  //$scope.$apply(function() {
+			      $scope.progressPerCent = (evt.position/evt.totalSize)*100;
+			  //});
+		  }).success(function(data, status, headers, config) {
+		    // file is uploaded successfully
+		    console.log(data);
+		  }).error(function() {
+		    console.log("Error on web video upload");
+		  });
+	  	}
+	}
+}]);
+
+/** Factories **/
+
+/* factory to get the video list from the server */
+(function() {
+  'use strict';
+ 
+  app.factory('GetUploadedVideosFactory', ['$http', 'colabConfig',
+    function($http, colabConfig) {
+      var _factory = {};
+ 
+      _factory.getVideoList = function() {
+        return $http.get(colabConfig.colabServerBaseURL + '/video_list');
+      };
+ 
+      return _factory;
+    }
+  ]);
+ 
+}());
+
+
+/** Modules **/
+
+/* module for general app config that can be injected as a dependency where needed */
+angular.module('colabConfig', [])
+  .value('colabConfig', {
+	  // Set the base URL for the colab server to be the host URL for the server hosting
+	  // Angular in this case. If you wanted to host the Angular and the node.js ColabSrever on
+	  //different server, to allow them scale separatly for example, his can simply be changed.
+	  var hosturl = $location.host()
+	  colabServerBaseURL: hosturl
+});
